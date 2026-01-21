15ebccb6ff007843353983ec14ab5e349e9e47c6
Daniel Martínez <mfdanielmf@gmail.com>
2026-01-22 00:43:23 +0100

    feat: añadir config pydantic (#9)
    * feat: añadir config pydantic
    
    * refactor: modificar tests y excepciones

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,23 +1,27 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+from app.config import Config
 
-DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:"
-    f"{POSTGRES_PASSWORD}@"
-    f"{POSTGRES_HOST}:"
-    f"{POSTGRES_PORT}/"
-    f"{POSTGRES_DB}"
-)
+# POSTGRES_USER = os.getenv("POSTGRES_USER")
+# POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+# POSTGRES_DB = os.getenv("POSTGRES_DB")
+# POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+# POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = (
+#     f"postgresql://{POSTGRES_USER}:"
+#     f"{POSTGRES_PASSWORD}@"
+#     f"{POSTGRES_HOST}:"
+#     f"{POSTGRES_PORT}/"
+#     f"{POSTGRES_DB}"
+# )
+
+
+config = Config()
+
+engine = create_engine(config.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database.db import get_db
 from app.models.user import User
+from app.models.exceptions import CorreoYaUsadoException, NombreYaUsadoException
 from app.schemas.user_schemas import UserCreate, UserReturn
 from app.schemas.auth_schemas import RegisterResponse
 from app.services.user_service import crear_usuario
@@ -12,9 +13,14 @@ auth_router = APIRouter()
 
 @auth_router.post("/register", response_model=RegisterResponse)
 def register(usuario: UserCreate, db: Session = Depends(get_db)):
-    usuario_db: User = crear_usuario(usuario=usuario, db=db)
+    try:
+        usuario_db: User = crear_usuario(usuario=usuario, db=db)
 
-    return {
-        "msg": "Usuario creado con exito",
-        "usuario": UserReturn.model_validate(usuario_db)
-    }
+        return {
+            "msg": "Usuario creado con exito",
+            "usuario": UserReturn.model_validate(usuario_db)
+        }
+    except CorreoYaUsadoException as e1:
+        raise HTTPException(status_code=400, detail=str(e1))
+    except NombreYaUsadoException as e2:
+        raise HTTPException(status_code=400, detail=str(e2))
