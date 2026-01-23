e48bb1d078a781d4ff8d0bcca8726ef86ab9a4b3
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-01-23 01:57:30 +0100

    feat: añadir endpoint me

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,14 +1,15 @@
 
 from datetime import datetime, timedelta, timezone
+import uuid
 from sqlalchemy.orm import Session
-from jose import jwt
+from jose import JWTError, jwt
 
 from app.config import config
 
 from app.models.user import User
 from app.services.user_service import comprobar_hash_contraseña, obtener_usuario_nombre
 from app.schemas.auth_schemas import LoginRequest, TokenData
-from app.models.exceptions import ContraseñaIncorrectaException
+from app.models.exceptions import UsuarioNoEncontradoException, ContraseñaIncorrectaException, UsuarioNoAutenticadoException
 
 
 def login_usuario(usuario_req: LoginRequest, db: Session) -> tuple[str, User]:
@@ -44,3 +45,27 @@ def generar_access_token(data: TokenData):
         to_encode, config.JWT_SECRET_KEY, algorithm=config.ALGORITHM)
 
     return encoded_jwt
+
+
+async def obtener_usuario_jwt(token: str, db: Session) -> User:
+    """
+    UsuarioNoEncontradoException, UsuarioNoAutenticadoException
+    """
+
+    try:
+        payload = jwt.decode(token, config.JWT_SECRET_KEY,
+                             algorithms=[config.ALGORITHM])
+        id: uuid.UUID = payload.get("id")
+        nombre_usuario: str = payload.get("nombre")
+        correo: str = payload.get("correo")
+
+        if nombre_usuario is None:
+            raise UsuarioNoEncontradoException()
+
+        token_data = TokenData(id=id, nombre=nombre_usuario, correo=correo)
+    except JWTError:
+        raise UsuarioNoAutenticadoException()
+
+    user: User = obtener_usuario_nombre(nombre=token_data.nombre, db=db)
+
+    return user
