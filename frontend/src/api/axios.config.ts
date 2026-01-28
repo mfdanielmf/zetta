627e99d5b458b46eb35f0beffeef00bd60ad1af3
Daniel Martínez Ferández <mfdanielmf@gmail.com>
2026-01-28 21:47:26 +0100

    refactor: migrar a openapi typescript

--- frontend/src/api/axios.config.ts
+++ frontend/src/api/axios.config.ts
@@ -1,15 +1,10 @@
-import Axios from 'axios';
-import type { AxiosRequestConfig } from 'axios';
+import axios from 'axios'
 
-export const AXIOS_INSTANCE = Axios.create({
-  baseURL: 'http://localhost:8080',
-  withCredentials: true
-});
+export default (url = 'http://localhost:8080') => {
+  const api = axios.create({
+    baseURL: url,
+    withCredentials: true
+  })
 
-export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
-  const promise = AXIOS_INSTANCE({ ...config }).then(({ data }) => data);
-
-  return promise;
-};
-
-export default customInstance;
+  return api
+}
