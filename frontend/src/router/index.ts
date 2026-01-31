d903280b7a55a67ef7bee7ce4bde7075e9f04381
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-01-31 02:44:35 +0100

    feat: añadir estructura visual y validación formulario registro

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainLayout from '@/layouts/MainLayout.vue'
 
+const AuthLayout = () => import("@/layouts/AuthLayout.vue")
+
+const RegisterView = () => import("@/views/auth/RegisterView.vue")
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,6 +13,19 @@ const router = createRouter({
       name: 'home',
       component: MainLayout
     },
+    {
+      path: "/auth",
+      redirect: {name: "register"}, //Temporal mientras no hago el login. Mejor redirigir a Login
+      component: AuthLayout,
+      children: [
+        {
+          path: "register",
+          name: "register",
+          component: RegisterView
+        }
+      ]
+    }
+
   ],
 })
 
