844ea0f838f36aaf2c968814aeaf92e755031c00
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-02-15 02:18:18 +0100

    feat: invalidar query al cambiar de cuenta

--- frontend/src/queries/useFilesQuery.ts
+++ frontend/src/queries/useFilesQuery.ts
@@ -1,9 +1,13 @@
 import { getFilesUserService } from "@/services/file.services";
+import { useAuthStore } from "@/stores/auth.store";
 import { useQuery } from "@tanstack/vue-query";
 
 export function useGetFilesUser(){
+  const authStore = useAuthStore()
+
   return useQuery({
-    queryKey: ["archivos"],
-    queryFn: () => getFilesUserService()
+    queryKey: ["archivos", authStore.usuario?.id],
+    queryFn: () => getFilesUserService(),
+    enabled: !!authStore.usuario?.id
   })
 }
