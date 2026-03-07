060f357ddf9afa3a1c6a0213f6bed18473e2cfe3
Daniel Martínez <mfdanielmf@gmail.com>
2026-03-07 19:48:48 +0100

    feat: implementar backend obtener carpetas usuario (#38)
    * feat: implementar backend obtener carpetas usuario
    
    * feat: añadir testing ver carpetas

--- backend/app/repositories/folder_repo.py
+++ backend/app/repositories/folder_repo.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from app.models.folder import Folder
 from app.models.user import User
 
+
 def add_folder(carpeta: Folder, db: Session) -> Folder:
     db.add(carpeta)
     db.commit()
@@ -12,8 +13,14 @@ def add_folder(carpeta: Folder, db: Session) -> Folder:
 
     return carpeta
 
+
 def get_folder_id_user(id: uuid.UUID, usuario: User, db: Session) -> Folder | None:
     return db.query(Folder).filter_by(id=id, id_usuario=usuario.id).first()
 
+
 def get_folder_original_name(nombre_original: str, usuario: User, db: Session) -> Folder | None:
     return db.query(Folder).filter_by(nombre_original=nombre_original, id_usuario=usuario.id).first()
+
+
+def get_folders_user(id_usuario: uuid.UUID, db: Session) -> list[Folder]:
+    return db.query(Folder).filter_by(id_usuario=id_usuario).all()
--- backend/app/routes/folder_routes.py
+++ backend/app/routes/folder_routes.py
@@ -6,7 +6,7 @@ from app.models.exceptions import IdYaUsadaException, NombreYaUsadoException
 from app.models.folder import Folder
 from app.models.user import User
 from app.services.auth_services import get_current_user
-from app.services.folder_services import crear_carpeta
+from app.services.folder_services import crear_carpeta, obtener_carpetas_usuario
 from app.schemas.folder_schemas import FolderBase, FolderRequest, FolderResponse
 
 folder_router = APIRouter()
@@ -33,3 +33,20 @@ def create_folder(req: FolderRequest, db: Session = Depends(get_db), usuario: Us
         raise HTTPException(409, str(e))
     except NombreYaUsadoException as e2:
         raise HTTPException(409, str(e2))
+
+
+@folder_router.get("", response_model=list[FolderBase])
+def get_files(db: Session = Depends(get_db), usuario: User = Depends(get_current_user)):
+    carpetas: list[Folder] = obtener_carpetas_usuario(usuario=usuario, db=db)
+
+    return [
+        FolderBase(
+            id=carpeta.id,
+            nombre_original=carpeta.nombre_original,
+            path=carpeta.path,
+            fecha_creacion=carpeta.fecha_creacion,
+            id_usuario=carpeta.id_usuario,
+            nombre_usuario=carpeta.usuario.nombre
+        )
+        for carpeta in carpetas
+    ]
