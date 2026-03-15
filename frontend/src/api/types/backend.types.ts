f4083f88a1c9d92772e8c2319fac6a8e2619ad6f
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-03-15 20:55:31 +0100

    feat: actualizar tipado backend

--- frontend/src/api/types/backend.types.ts
+++ frontend/src/api/types/backend.types.ts
@@ -142,6 +142,24 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/folders/{id_carpeta}/files": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Get Files Of Folder */
+        get: operations["get_files_of_folder_api_folders__id_carpeta__files_get"];
+        put?: never;
+        /** Upload File To Folder */
+        post: operations["upload_file_to_folder_api_folders__id_carpeta__files_post"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -150,6 +168,14 @@ export interface components {
         Body_upload_file_api_files_post: {
             /** File Upload */
             file_upload: string[];
+        };
+        /** Body_upload_file_to_folder_api_folders__id_carpeta__files_post */
+        Body_upload_file_to_folder_api_folders__id_carpeta__files_post: {
+            /**
+             * Archivo
+             * Format: binary
+             */
+            archivo: string;
         };
         /** FileBase */
         FileBase: {
@@ -174,6 +200,8 @@ export interface components {
              * Format: uuid
              */
             id_usuario: string;
+            /** Id Carpeta */
+            id_carpeta: string | null;
             /** Nombre Usuario */
             nombre_usuario: string;
         };
@@ -251,6 +279,12 @@ export interface components {
             /** Msg */
             msg: string;
             usuario: components["schemas"]["UserReturn"];
+        };
+        /** UploadFileFolderResponse */
+        UploadFileFolderResponse: {
+            /** Msg */
+            msg: string;
+            archivo: components["schemas"]["FileBase"];
         };
         /** UserCreate */
         UserCreate: {
@@ -577,6 +611,76 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["FolderResponse"];
+                };
+            };
+            /** @description Validation Error */
+            422: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["HTTPValidationError"];
+                };
+            };
+        };
+    };
+    get_files_of_folder_api_folders__id_carpeta__files_get: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                id_carpeta: string;
+            };
+            cookie?: {
+                access_token?: string;
+            };
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["FileBase"][];
+                };
+            };
+            /** @description Validation Error */
+            422: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["HTTPValidationError"];
+                };
+            };
+        };
+    };
+    upload_file_to_folder_api_folders__id_carpeta__files_post: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                id_carpeta: string;
+            };
+            cookie?: {
+                access_token?: string;
+            };
+        };
+        requestBody: {
+            content: {
+                "multipart/form-data": components["schemas"]["Body_upload_file_to_folder_api_folders__id_carpeta__files_post"];
+            };
+        };
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["UploadFileFolderResponse"];
                 };
             };
             /** @description Validation Error */
