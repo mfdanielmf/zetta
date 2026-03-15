39ec776966ccbab211f35dea07b5c6b8047927bd
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-03-15 19:27:26 +0100

    feat: añadir tests tamaño excedido, nombre usado, carpeta no existente y subir sin archivo

--- backend/tests/folders/test_upload_file_folder.py
+++ backend/tests/folders/test_upload_file_folder.py
@@ -5,6 +5,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 from app.models.file import File
 from app.models.user import User
+from app.models.exceptions import CarpetaNoEncontradaException, TamañoExcedidoException, NombreYaUsadoException
 from app.services.auth_services import get_current_user
 from app.schemas.folder_schemas import UploadFileFolderResponse
 from tests.util import override_get_current_user
@@ -47,5 +48,73 @@ def test_subir_archivo_carpeta():
     assert json_response.archivo.id_usuario == usuario.id
     assert json_response.archivo.nombre_original == "testing.txt"
     assert json_response.archivo.id_carpeta == id_carpeta
+
+    app.dependency_overrides.clear()
+
+
+def test_subir_archivo_carpeta_no_existente():
+    app.dependency_overrides[get_current_user] = override_get_current_user
+    id_carpeta: uuid.UUID = uuid.uuid4()
+
+    with patch("app.routes.folder_routes.guardar_archivo_carpeta", new_callable=AsyncMock) as mock_guardar:
+        mock_guardar.side_effect = CarpetaNoEncontradaException(
+            f"No se ha encontrado la carpeta con id {id_carpeta}")
+
+        response = client.post(f"/api/folders/{id_carpeta}/files", files={
+            "archivo": ("testing.txt", io.BytesIO(b"Test"), "text/plain")
+        })
+
+    assert response.status_code == 404
+    assert response.json()[
+        "detail"] == f"No se ha encontrado la carpeta con id {id_carpeta}"
+
+    app.dependency_overrides.clear()
+
+
+def test_subir_sin_archivo_carpeta():
+    app.dependency_overrides[get_current_user] = override_get_current_user
+
+    id_carpeta: uuid.UUID = uuid.uuid4()
+
+    response = client.post(f"/api/folders/{id_carpeta}/files")
+
+    assert response.status_code == 422
+
+    app.dependency_overrides.clear()
+
+
+def test_subir_archivo_carpeta_tamaño_excedido():
+    app.dependency_overrides[get_current_user] = override_get_current_user
+    id_carpeta: uuid.UUID = uuid.uuid4()
+
+    with patch("app.routes.folder_routes.guardar_archivo_carpeta", new_callable=AsyncMock) as mock_guardar:
+        mock_guardar.side_effect = TamañoExcedidoException(
+            "Has superado el tamaño límite")
+
+        response = client.post(f"/api/folders/{id_carpeta}/files", files={
+            "archivo": ("testing.txt", io.BytesIO(b"Test"), "text/plain")
+        })
+
+    assert response.status_code == 413
+    assert response.json()["detail"] == "Has superado el tamaño límite"
+
+    app.dependency_overrides.clear()
+
+
+def test_subir_archivo_nombre_usado():
+    app.dependency_overrides[get_current_user] = override_get_current_user
+    id_carpeta: uuid.UUID = uuid.uuid4()
+
+    with patch("app.routes.folder_routes.guardar_archivo_carpeta", new_callable=AsyncMock) as mock_guardar:
+        mock_guardar.side_effect = NombreYaUsadoException(
+            "Ya hay un archivo con el nombre 'testing.txt' en la carpeta")
+
+        response = client.post(f"/api/folders/{id_carpeta}/files", files={
+            "archivo": ("testing.txt", io.BytesIO(b"Test"), "text/plain")
+        })
+
+    assert response.status_code == 409
+    assert response.json()[
+        "detail"] == "Ya hay un archivo con el nombre 'testing.txt' en la carpeta"
 
     app.dependency_overrides.clear()
