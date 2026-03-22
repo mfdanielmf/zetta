c72aedd484835a88b95f06ffac326face2240442
Daniel Martínez <mfdanielmf@gmail.com>
2026-03-22 16:11:21 +0100

    feat: crear carpetas anidadas backend (#55)
    * feat: añadir id carpeta padre y ejecutar migraciones
    
    * feat: añadir funcionalidad crear carpetas anidadas
    
    * refactor: pasar obtener todas las carpetas usuario a raiz
    
    * refactor: adaptar mock test a la nueva función de la ruta
    
    * fix: añadir filter carpeta none a repo carpeta raíz
    
    * feat: añadir tests endpoint

--- backend/app/repositories/folder_repo.py
+++ backend/app/repositories/folder_repo.py
@@ -24,3 +24,15 @@ def get_folder_original_name(nombre_original: str, usuario: User, db: Session) -
 
 def get_folders_user(id_usuario: uuid.UUID, db: Session) -> list[Folder]:
     return db.query(Folder).filter_by(id_usuario=id_usuario).all()
+
+
+def get_folder_name_anidada(id_carpeta_padre: str, nombre_carpeta: str, usuario: User, db: Session) -> Folder | None:
+    return db.query(Folder).filter_by(id_usuario=usuario.id, nombre_original=nombre_carpeta, id_carpeta=id_carpeta_padre).first()
+
+
+def get_folder_id(id_carpeta: str, db: Session) -> Folder | None:
+    return db.query(Folder).filter_by(id=id_carpeta).first()
+
+
+def get_folders_user_raiz(id_usuario: uuid.UUID, db: Session) -> list[Folder]:
+    return db.query(Folder).filter_by(id_usuario=id_usuario, id_carpeta=None).all()
--- backend/app/services/folder_services.py
+++ backend/app/services/folder_services.py
@@ -9,7 +9,7 @@ from app.models.exceptions import IdYaUsadaException, NombreYaUsadoException, Ca
 from app.models.file import File
 from app.models.folder import Folder
 from app.models.user import User
-from app.repositories.folder_repo import add_folder, get_folder_id_user, get_folder_original_name, get_folders_user
+from app.repositories.folder_repo import add_folder, get_folder_id_user, get_folder_original_name, get_folders_user, get_folder_name_anidada, get_folder_id, get_folders_user_raiz
 from app.repositories.file_repo import insert_file_db, get_file_by_name_in_folder
 
 UPLOAD_DIR = Path(config.UPLOAD_DIR)
@@ -27,7 +27,7 @@ def crear_carpeta(nombre: str, usuario: User, db: Session) -> Folder:
 
     id_carpeta: uuid.UUID = uuid.uuid4()
 
-    if get_folder_id_user(id=id_carpeta, usuario=usuario, db=db) is not None:
+    if get_folder_id(id=id_carpeta, db=db) is not None:
         raise IdYaUsadaException(
             f"Ya se ha usado la ID {id}. Vuelve a subir la carpeta")
 
@@ -47,6 +47,10 @@ def crear_carpeta(nombre: str, usuario: User, db: Session) -> Folder:
 
 def obtener_carpetas_usuario(usuario: User, db: Session) -> list[Folder]:
     return get_folders_user(id_usuario=usuario.id, db=db)
+
+
+def obtener_carpetas_usuario_raiz(usuario: User, db: Session) -> list[Folder]:
+    return get_folders_user_raiz(id_usuario=usuario.id, db=db)
 
 
 def obtener_carpeta_usuario_id(id_carpeta: str, usuario: User, db: Session) -> Folder:
@@ -116,3 +120,35 @@ async def guardar_archivo_carpeta(id_carpeta: str, file_upload: UploadFile, db: 
     subir_archivo_carpeta_disco(file_path=file_path, data=data)
 
     return archivo_guardado
+
+
+def crear_carpeta_anidada(id_carpeta_padre: str, nombre: str, usuario: User, db: Session) -> Folder:
+    """
+    CarpetaNoEncontradaException, NombreYaUsadoException, IdYaUsadaException
+    """
+    if not get_folder_id_user(id=id_carpeta_padre, usuario=usuario, db=db):
+        raise CarpetaNoEncontradaException(
+            f"No se ha encontrado la carpeta con ID {id_carpeta_padre}")
+
+    if get_folder_name_anidada(id_carpeta_padre=id_carpeta_padre, nombre_carpeta=nombre, usuario=usuario, db=db):
+        raise NombreYaUsadoException(
+            f"Ya has creado una carpeta con el nombre {nombre}")
+
+    id_carpeta_nueva: uuid.UUID = uuid.uuid4()
+
+    if get_folder_id(id_carpeta=id_carpeta_nueva, db=db) is not None:
+        raise IdYaUsadaException(
+            f"Ya se ha usado la ID {id}. Vuelve a crear la carpeta")
+
+    # Crear el almacén del usuario si no existe
+    ruta_usuario = UPLOAD_DIR / str(usuario.id)
+    ruta_usuario.mkdir(parents=True, exist_ok=True)
+
+    folder_path = ruta_usuario / str(id_carpeta_padre) / str(id_carpeta_nueva)
+    folder_path.mkdir()
+
+    carpeta: Folder = Folder(id=id_carpeta_nueva, nombre_original=nombre, path=str(
+        folder_path), id_usuario=usuario.id, id_carpeta=id_carpeta_padre)
+    carpeta_db: Folder = add_folder(carpeta=carpeta, db=db)
+
+    return carpeta_db
