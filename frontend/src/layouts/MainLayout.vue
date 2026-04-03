5439fb260594c0a8275a9e827fbfe7f7902c30a7
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-04-03 21:11:43 +0200

    feat: añadir papelera al breadcrumb

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -10,8 +10,25 @@ import BreadcrumbSeparator from '@/components/ui/breadcrumb/BreadcrumbSeparator.
 import { Separator } from '@/components/ui/separator'
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { useFolderStore } from '@/stores/folder.store'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
 const carpetasStore = useFolderStore()
+
+const mainBreadcrumb = computed(() => {
+  if (route.name === 'papelera' || route.path.startsWith('/trash')) {
+    return {
+      name: 'papelera',
+      titulo: 'Papelera',
+    }
+  } else {
+    return {
+      name: 'archivos',
+      titulo: 'Mis Archivos',
+    }
+  }
+})
 </script>
 
 <template>
@@ -28,7 +45,9 @@ const carpetasStore = useFolderStore()
             <BreadcrumbList>
               <BreadcrumbItem>
                 <BreadcrumbLink as-child>
-                  <RouterLink :to="{ name: 'archivos' }">Mis Archivos</RouterLink>
+                  <RouterLink :to="{ name: mainBreadcrumb.name }">
+                    {{ mainBreadcrumb.titulo }}
+                  </RouterLink>
                 </BreadcrumbLink>
               </BreadcrumbItem>
 
