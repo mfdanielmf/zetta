f155d784a4b9920d6283b744206496893024027a
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-04-18 17:14:19 +0200

    refactor: obtener archivos raiz si no estan en la papelera

--- backend/app/repositories/file_repo.py
+++ backend/app/repositories/file_repo.py
@@ -47,10 +47,11 @@ def get_file_trash(id_archivo: uuid.UUID, id_usuario: uuid.UUID, db: Session) ->
 def get_all_files_trash_raiz(id_usuario: uuid.UUID, db: Session) -> list[File]:
     return db.query(File).filter(File.id_usuario == id_usuario, File.fecha_eliminacion != None, File.id_carpeta == None).all()
 
+
 def get_files_raiz(id_usuario: uuid.UUID, db: Session) -> list[File]:
-    return db.query(File).filter(File.id_usuario == id_usuario, File.id_carpeta == None).all()
+    return db.query(File).filter(File.id_usuario == id_usuario, File.id_carpeta == None, File.fecha_eliminacion == None).all()
+
 
 def delete_file(archivo: File, db: Session):
     db.delete(archivo)
     db.commit()
-    
