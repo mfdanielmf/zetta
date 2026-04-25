4cdf81b2ed832274d4b2547a5f16cde8307d5999
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-04-25 16:30:12 +0200

    feat: añadir tests carpetas compartidas por usuario

--- backend/tests/shared/test_shared_folder_by_me.py
+++ backend/tests/shared/test_shared_folder_by_me.py
@@ -2,11 +2,10 @@ import uuid
 
 from fastapi.testclient import TestClient
 from app.main import app
-from app.models.archivo_compartido import ArchivoCompartido
-from app.models.file import File
 from app.models.user import User
-from app.schemas import shared_file_schemas
-from app.schemas.file_schemas import FileBase
+from app.schemas import shared_folder_schemas
+from app.schemas.folder_schemas import FolderBase
+from app.schemas.shared_folder_schemas import CarpetaCompartidaBase
 from app.services.auth_services import get_current_user
 from tests.util import override_get_current_user
 from unittest.mock import patch
@@ -14,18 +13,17 @@ from unittest.mock import patch
 client = TestClient(app=app)
 
 
-def test_archivos_compartidos_por_mi_success():
+def test_carpetas_compartidos_por_mi_success():
     usuario = override_get_current_user()
     app.dependency_overrides[get_current_user] = lambda: usuario
 
     id_compartido: uuid.UUID = uuid.uuid4()
-    id_archivo: uuid.UUID = uuid.uuid4()
+    id_carpeta: uuid.UUID = uuid.uuid4()
 
-    archivo_falso: File = FileBase(
-        id=id_archivo,
+    carpeta_falsa: FolderBase = FolderBase(
+        id=id_carpeta,
         nombre_original="testing.txt",
         path="uploads/testing.txt",
-        tamaño_bytes=10,
         fecha_creacion="2026-02-15T10:00:00",
         id_usuario=usuario.id,
         usuario=usuario,
@@ -40,18 +38,18 @@ def test_archivos_compartidos_por_mi_success():
         fecha_creacion="2026-01-21T01:44:31.825198"
     )
 
-    archivo_compartido: ArchivoCompartido = shared_file_schemas.ArchivoCompartidoBase(
+    carpeta_compartida: CarpetaCompartidaBase = shared_folder_schemas.CarpetaCompartidaBase(
         id=id_compartido,
         fecha_compartido="2026-02-15T10:00:00",
         propietario=usuario,
         receptor=usuario_falso2,
-        archivo=archivo_falso
+        carpeta=carpeta_falsa
     )
 
-    with patch("app.routes.shared_routes.shared_file_services.obtener_archivos_compartidos") as mock_compartidos:
-        mock_compartidos.return_value = [archivo_compartido]
+    with patch("app.routes.shared_routes.shared_folder_services.obtener_carpetas_compartidas") as mock_compartidos:
+        mock_compartidos.return_value = [carpeta_compartida]
 
-        response = client.get("/api/shared/sent/files")
+        response = client.get("/api/shared/sent/folders")
 
     resp_json = response.json()
     assert response.status_code == 200
@@ -61,22 +59,21 @@ def test_archivos_compartidos_por_mi_success():
     assert resp_json[0]["propietario"]["correo"] == usuario.correo
     assert resp_json[0]["receptor"]["id"] == str(usuario_falso2.id)
     assert resp_json[0]["receptor"]["correo"] == usuario_falso2.correo
-    assert resp_json[0]["archivo"]["id"] == str(archivo_falso.id)
-    assert resp_json[0]["archivo"]["nombre_original"] == archivo_falso.nombre_original
-    assert resp_json[0]["archivo"]["path"] == archivo_falso.path
-    assert resp_json[0]["archivo"]["tamaño_bytes"] == archivo_falso.tamaño_bytes
-    assert resp_json[0]["archivo"]["id_usuario"] == str(usuario.id)
+    assert resp_json[0]["carpeta"]["id"] == str(carpeta_falsa.id)
+    assert resp_json[0]["carpeta"]["nombre_original"] == carpeta_falsa.nombre_original
+    assert resp_json[0]["carpeta"]["path"] == carpeta_falsa.path
+    assert resp_json[0]["carpeta"]["id_usuario"] == str(usuario.id)
 
     app.dependency_overrides.clear()
 
 
-def test_archivos_compartidos_por_mi_vacio():
+def test_carpetas_compartidas_por_mi_vacio():
     app.dependency_overrides[get_current_user] = override_get_current_user
 
-    with patch("app.routes.shared_routes.shared_file_services.obtener_archivos_compartidos") as mock_compartidos:
+    with patch("app.routes.shared_routes.shared_folder_services.obtener_carpetas_compartidas") as mock_compartidos:
         mock_compartidos.return_value = []
 
-        response = client.get("/api/shared/sent/files")
+        response = client.get("/api/shared/sent/folders")
 
     resp_json = response.json()
     assert response.status_code == 200
