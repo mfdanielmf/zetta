31388c5e1a1535baf2e7d53f92ba2a3473f2fc57
Daniel Martínez <mfdanielmf@gmail.com>
2026-05-03 17:37:40 +0200

    refactor: unificar detalles carpeta backend (#106)

--- backend/app/services/item_services.py
+++ backend/app/services/item_services.py
@@ -1,4 +1,5 @@
 from typing import Union
+from uuid import UUID
 
 from sqlalchemy.orm import Session
 
@@ -6,6 +7,7 @@ from app.models.file import File
 from app.models.folder import Folder
 from app.models.user import User
 from app.repositories import file_repo, folder_repo
+from app.services import folder_services
 
 
 def obtener_items_raiz_paginados(usuario: User, db: Session, pagina: int, limite: int) -> tuple[int, list[Union[Folder, File]]]:
@@ -15,6 +17,29 @@ def obtener_items_raiz_paginados(usuario: User, db: Session, pagina: int, limite
         id_usuario=usuario.id, db=db)
     archivos: list[File] = file_repo.get_files_raiz_sorted(
         id_usuario=usuario.id, db=db)
+
+    items: list[Union[Folder, File]] = carpetas + archivos
+
+    total: int = len(items)
+
+    items_paginados = items[offset: offset + limite]
+
+    return total, items_paginados
+
+
+def obtener_items_carpeta_paginados(id_carpeta: UUID, usuario: User, db: Session, pagina: int, limite: int) -> tuple[int, list[Union[Folder, File]]]:
+    """
+    CarpetaNoEncontradaException
+    """
+    offset: int = (pagina - 1) * limite
+
+    folder_services.obtener_carpeta_usuario_id(
+        id_carpeta=id_carpeta, usuario=usuario, db=db)
+
+    carpetas: list[Folder] = folder_repo.get_folders_inside_folder_sorted(
+        id_carpeta=id_carpeta, id_usuario=usuario.id, db=db)
+    archivos: list[File] = file_repo.get_all_files_in_folder_sorted(id_carpeta=id_carpeta,
+                                                                    id_usuario=usuario.id, db=db)
 
     items: list[Union[Folder, File]] = carpetas + archivos
 
