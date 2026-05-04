a93bb975da2975c364407f0dd917dc2e815192bc
Daniel Martínez <mfdanielmf@gmail.com>
2026-05-04 18:55:38 +0200

    refactor: unificar archivos y carpetas backend (#110)

--- backend/app/routes/item_routes.py
+++ backend/app/routes/item_routes.py
@@ -64,7 +64,7 @@ def get_items(usuario: User = Depends(get_current_user), db: Session = Depends(g
 
 
 @item_router.get("/{id_carpeta}/items", response_model=item_schemas.PaginatedItemResponse)
-def get_items(id_carpeta: UUID, usuario: User = Depends(get_current_user), db: Session = Depends(get_db), paginacion: tuple[int, int] = Depends(get_pagination)):
+def get_items_folder(id_carpeta: UUID, usuario: User = Depends(get_current_user), db: Session = Depends(get_db), paginacion: tuple[int, int] = Depends(get_pagination)):
     pagina, limite = paginacion
 
     try:
@@ -115,12 +115,59 @@ def get_items(id_carpeta: UUID, usuario: User = Depends(get_current_user), db: S
 
 
 @item_router.get("/trash", response_model=item_schemas.PaginatedItemResponse)
-def get_items(usuario: User = Depends(get_current_user), db: Session = Depends(get_db), paginacion: tuple[int, int] = Depends(get_pagination)):
+def get_items_folder_in_trash(usuario: User = Depends(get_current_user), db: Session = Depends(get_db), paginacion: tuple[int, int] = Depends(get_pagination)):
+    pagina, limite = paginacion
+
+    total, items = item_services.obtener_items_papelera_paginados(
+        usuario=usuario, db=db, pagina=pagina, limite=limite)
+
+    items_serializados: list[Union[file_schemas.FileBase,
+                                   file_schemas.File]] = []
+
+    for item in items:
+        if isinstance(item, File):
+            items_serializados.append(
+                item_schemas.FileItem(
+                    id=item.id,
+                    nombre_original=item.nombre_original,
+                    path=item.path,
+                    tamaño_bytes=item.tamaño_bytes,
+                    fecha_creacion=item.fecha_creacion,
+                    id_usuario=item.id_usuario,
+                    nombre_usuario=item.usuario.nombre,
+                    id_carpeta=item.id_carpeta,
+                    fecha_eliminacion=item.fecha_eliminacion
+                )
+            )
+        else:
+            items_serializados.append(
+                item_schemas.FolderItem(
+                    id=item.id,
+                    nombre_original=item.nombre_original,
+                    path=item.path,
+                    fecha_creacion=item.fecha_creacion,
+                    id_usuario=item.id_usuario,
+                    nombre_usuario=item.usuario.nombre,
+                    id_carpeta=item.id_carpeta,
+                    fecha_eliminacion=item.fecha_eliminacion
+                )
+            )
+
+    return {
+        "items": items_serializados,
+        "total": total,
+        "pagina": pagina,
+        "limite": limite
+    }
+
+
+@item_router.get("/trash/{id_carpeta}/items", response_model=item_schemas.PaginatedItemResponse)
+def get_items(id_carpeta: UUID, usuario: User = Depends(get_current_user), db: Session = Depends(get_db), paginacion: tuple[int, int] = Depends(get_pagination)):
     pagina, limite = paginacion
 
     try:
-        total, items = item_services.obtener_items_papelera_paginados(
-            usuario=usuario, db=db, pagina=pagina, limite=limite)
+        total, items = item_services.obtener_items_carpeta_papelera_paginados(id_carpeta=id_carpeta,
+                                                                              usuario=usuario, db=db, pagina=pagina, limite=limite)
 
         items_serializados: list[Union[file_schemas.FileBase,
                                        file_schemas.File]] = []
@@ -161,5 +208,6 @@ def get_items(usuario: User = Depends(get_current_user), db: Session = Depends(g
             "limite": limite
         }
 
-    except ex.CarpetaNoEncontradaException as e1:
-        raise HTTPException(404, detail=str(e1))
+    except ex.CarpetaNoEncontradaException:
+        raise HTTPException(
+            404, detail=f"No se ha encontrado la carpeta con id {id_carpeta} en la papelera")
--- backend/app/services/item_services.py
+++ backend/app/services/item_services.py
@@ -65,3 +65,26 @@ def obtener_items_papelera_paginados(usuario: User, db: Session, pagina: int, li
     items_paginados = items[offset: offset + limite]
 
     return total, items_paginados
+
+
+def obtener_items_carpeta_papelera_paginados(id_carpeta: UUID, usuario: User, db: Session, pagina: int, limite: int) -> tuple[int, list[Union[Folder, File]]]:
+    """
+    CarpetaNoEncontradaException
+    """
+    offset: int = (pagina - 1) * limite
+
+    folder_services.obtener_carpeta_papelera(
+        id_carpeta=id_carpeta, usuario=usuario, db=db)
+
+    carpetas: list[Folder] = folder_repo.get_folders_inside_folder_sorted(
+        id_carpeta=id_carpeta, id_usuario=usuario.id, db=db)
+    archivos: list[File] = file_repo.get_all_files_in_folder_sorted(id_carpeta=id_carpeta,
+                                                                    id_usuario=usuario.id, db=db)
+
+    items: list[Union[Folder, File]] = carpetas + archivos
+
+    total: int = len(items)
+
+    items_paginados = items[offset: offset + limite]
+
+    return total, items_paginados
