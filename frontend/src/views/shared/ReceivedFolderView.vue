65fe28c52ca19fd5924a7957be75ddb23e4b3903
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-05-08 21:06:52 +0200

    feat: añadir paginación detalles carpeta recibida

--- frontend/src/views/shared/ReceivedFolderView.vue
+++ frontend/src/views/shared/ReceivedFolderView.vue
@@ -17,7 +17,19 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { useGetFilesFolder, useGetFoldersAnidadas } from '@/queries/useFoldersQuery'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationItem,
+  PaginationLast,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination'
+
+import config from '@/config/config'
+import { useGetFolderItems } from '@/queries/useItemsQuery'
 
 import {
   downloadFileService,
@@ -28,7 +40,7 @@ import { downloadFolderService } from '@/services/folder.services'
 import { useFolderStore } from '@/stores/folder.store'
 import getIconExtension from '@/utils/iconMap'
 import { Download, Ellipsis, Folder } from 'lucide-vue-next'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
@@ -37,28 +49,18 @@ const folderStore = useFolderStore()
 
 const idCarpeta = computed(() => route.params.id as string)
 
-const cargando = computed(() => {
-  if (loadingArchivos.value || loadingCarpetasAnidadas.value) {
-    return true
-  }
-
-  return false
-})
-
 const noData = computed(() => {
-  if (
-    (!dataArchivos.value || dataArchivos.value?.length < 1) &&
-    (!dataCarpetasAnidadas.value || dataCarpetasAnidadas.value?.length < 1)
-  ) {
+  if (!dataItems.value || dataItems.value.total < 1) {
     return true
   }
 
   return false
 })
 
-const { data: dataArchivos, isLoading: loadingArchivos } = useGetFilesFolder(idCarpeta)
-const { data: dataCarpetasAnidadas, isLoading: loadingCarpetasAnidadas } =
-  useGetFoldersAnidadas(idCarpeta)
+const pagina = ref<number>(1)
+const limite = config.LIMITE_FETCH
+
+const { data: dataItems, isLoading: loadingItems } = useGetFolderItems(idCarpeta, pagina, limite)
 
 async function descargarArchivo(id: string, nombre: string) {
   await downloadFileService(id, nombre)
@@ -78,8 +80,8 @@ async function descargarCarpeta(id: string, nombre: string) {
 <template>
   <div class="space-y-2">
     <Table>
-      <TableCaption v-if="cargando || noData">
-        {{ cargando ? 'Cargando...' : 'Esta carpeta no tiene contenido' }}
+      <TableCaption v-if="loadingItems || noData">
+        {{ loadingItems ? 'Cargando...' : 'Esta carpeta no tiene contenido' }}
       </TableCaption>
 
       <TableHeader class="bg-neutral-100">
@@ -93,25 +95,31 @@ async function descargarCarpeta(id: string, nombre: string) {
       </TableHeader>
 
       <TableBody v-if="!noData">
-        <!-- Carpetas -->
         <TableRow
-          v-for="folder in dataCarpetasAnidadas"
-          :key="folder.id"
-          class="hover:cursor-pointer"
-          @click="handleNavigationDetallesCarpeta(folder.id, folder.nombre_original)"
+          v-for="item in dataItems?.items"
+          :key="item.id"
+          @click="
+            item.tipo === 'folder'
+              ? handleNavigationDetallesCarpeta(item.id, item.nombre_original)
+              : null
+          "
+          :class="{ 'hover:cursor-pointer': item.tipo === 'folder' }"
         >
           <TableCell class="font-medium">
             <div class="flex items-center gap-2">
-              <Folder :size="20" />
-              {{ folder.nombre_original }}
+              <Folder :size="20" v-if="item.tipo === 'folder'" />
+              <component :is="getIconExtension(item.nombre_original)" :size="20" v-else />
+              {{ item.nombre_original }}
             </div>
           </TableCell>
           <TableCell class="font-medium">
-            {{ folder.nombre_usuario }}
+            {{ item.nombre_usuario }}
           </TableCell>
-          <TableCell class="font-medium"> - </TableCell>
           <TableCell class="font-medium">
-            {{ formatDateService(folder.fecha_creacion) }}
+            {{ item.tipo === 'file' ? formatearTamañoService(item.tamaño_bytes) : '-' }}
+          </TableCell>
+          <TableCell class="font-medium">
+            {{ formatDateService(item.fecha_creacion) }}
           </TableCell>
           <TableCell>
             <DropdownMenu>
@@ -125,46 +133,11 @@ async function descargarCarpeta(id: string, nombre: string) {
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   class="hover:cursor-pointer"
-                  @click="descargarCarpeta(folder.id, folder.nombre_original)"
-                >
-                  <Download />
-                  Descargar
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </TableCell>
-        </TableRow>
-
-        <!-- Archivos -->
-        <TableRow v-for="file in dataArchivos" :key="file.id">
-          <TableCell class="font-medium">
-            <div class="flex items-center gap-2">
-              <component :is="getIconExtension(file.nombre_original)" :size="20" />
-              {{ file.nombre_original }}
-            </div>
-          </TableCell>
-          <TableCell class="font-medium">
-            {{ file.nombre_usuario }}
-          </TableCell>
-          <TableCell class="font-medium">
-            {{ formatearTamañoService(file.tamaño_bytes) }}
-          </TableCell>
-          <TableCell class="font-medium">
-            {{ formatDateService(file.fecha_creacion) }}
-          </TableCell>
-          <TableCell>
-            <DropdownMenu>
-              <DropdownMenuTrigger as-child>
-                <Button variant="outline" size="icon" class="hover:cursor-pointer">
-                  <Ellipsis />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  class="hover:cursor-pointer"
-                  @click="descargarArchivo(file.id, file.nombre_original)"
+                  @click="
+                    item.tipo === 'folder'
+                      ? descargarCarpeta(item.id, item.nombre_original)
+                      : descargarArchivo(item.id, item.nombre_original)
+                  "
                 >
                   <Download />
                   Descargar
@@ -175,5 +148,33 @@ async function descargarCarpeta(id: string, nombre: string) {
         </TableRow>
       </TableBody>
     </Table>
+  </div>
+
+  <div class="flex flex-col gap-6 pt-10">
+    <Pagination
+      v-if="dataItems && dataItems.total > 0"
+      v-slot="{ page }"
+      :items-per-page="dataItems.limite"
+      :total="dataItems?.total"
+      v-model:page="pagina"
+    >
+      <PaginationContent v-slot="{ items }">
+        <PaginationPrevious class="hover:cursor-pointer" />
+        <PaginationFirst class="hover:cursor-pointer" />
+        <template v-for="(item, index) in items" :key="index">
+          <PaginationItem
+            v-if="item.type === 'page'"
+            :value="item.value"
+            :is-active="item.value === page"
+            class="hover:cursor-pointer"
+          >
+            {{ item.value }}
+          </PaginationItem>
+        </template>
+        <PaginationEllipsis :index="4" />
+        <PaginationLast class="hover:cursor-pointer" />
+        <PaginationNext class="hover:cursor-pointer" />
+      </PaginationContent>
+    </Pagination>
   </div>
 </template>
