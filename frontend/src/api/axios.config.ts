57a9c92ceb7c91078ff3462c3759fc6c886fdb41
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-05-10 19:45:20 +0200

    feat: añadir config para prod y favicon

--- frontend/src/api/axios.config.ts
+++ frontend/src/api/axios.config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export default (url = 'http://localhost:8080') => {
+export default (url = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080') => {
   const api = axios.create({
     baseURL: url,
     withCredentials: true,
