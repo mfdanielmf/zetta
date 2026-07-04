6290b5efb0c2481028372097999e055327fdfb38
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-07-04 20:19:00 +0200

    chore: migrar frontend a bun

--- frontend/src/api/types/backend.types.ts
+++ frontend/src/api/types/backend.types.ts
@@ -1195,6 +1195,10 @@ export interface components {
             msg: string;
             /** Error Type */
             type: string;
+            /** Input */
+            input?: unknown;
+            /** Context */
+            ctx?: Record<string, never>;
         };
     };
     responses: never;
